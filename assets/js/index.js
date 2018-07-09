c322d7b31ef2bd6d2c63609f16ef15a5d597c22d
Luke Moderwell <luke.moderwell@gmail.com>
2018-07-09 08:14:09 -0400

    testing bg color change

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,16 @@
-console.log(`Yes`);
+const appBg = document.getElementsByClassName('app-background')[0];
 
+document.addEventListener('scroll', function() {
+  console.log(window.scrollY);
+  switch (window.scrollY > 0) {
+    case true:
+      appBg.style.backgroundColor = `blue`;
+      break;
+    case false:
+      appBg.style.backgroundColor = `red`;
+      break;
+    default:
+      break;
+  }
+})
+  
