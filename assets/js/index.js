ed9aad773cebf33c383a63791a01c0113532009f
Luke Moderwell <luke.moderwell@gmail.com>
2018-07-17 23:41:36 -0400

    test bgImg JS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,25 @@
+// DOM
 const appBg = document.getElementsByClassName('app-background')[0];
+const appBgImg = document.getElementsByClassName('app-background-image')[0];
+const body = document.getElementsByTagName('body')[0];
 
 document.addEventListener('scroll', function() {
   console.log(window.scrollY);
-  switch (window.scrollY > 0) {
-    case true:
-      appBg.style.backgroundColor = `blue`;
-      break;
-    case false:
-      appBg.style.backgroundColor = `red`;
-      break;
-    default:
-      break;
+  
+  if (window.scrollY < 200 ) {
+    body.classList.remove('theme-dark');
+    appBg.style.backgroundColor = `whitesmoke`;
+    appBgImg.style.opacity = `0`;
+    appBgImg.style.backgroundImage = 
+      `url('https://images.unsplash.com/photo-1491821893533-80f535044695?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a57657772157dd7a5a6fb827ab9fba23&auto=format&fit=crop&w=934&q=80')`;
+  }
+
+  if (window.scrollY > 200 ) {
+    body.classList.add('theme-dark');
+    appBg.style.backgroundColor = `black`;
+    appBgImg.style.opacity = `.3`;
+    appBgImg.style.backgroundImage = 
+      `url('https://images.unsplash.com/photo-1491821893533-80f535044695?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a57657772157dd7a5a6fb827ab9fba23&auto=format&fit=crop&w=934&q=80')`;
   }
 })
   
