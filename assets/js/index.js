9034831a7b5a3f0f983142a770f712c1f409b64e
Luke Moderwell <luke.moderwell@gmail.com>
2018-07-23 13:08:48 -0400

    WIP: JS class

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,39 +1,58 @@
-// Colors
-const salmon = `#F19AA2`;
-const tomato = `#ED5342`;
-const baby = `#A2C8E8`;
-const gray = `#3A3132`;
-const black = `#131212`;
-const cream = `#EDE2D2`;
-const white = `#FFFAF2`;
+class WWA {
+  constructor() {
+    // Colors
+    this.salmon = `#F19AA2`;
+    this.tomato = `#ED5342`;
+    this.baby = `#A2C8E8`;
+    this.cream = `#EDE2D2`;
+    
+    // Content
+    this.bgImg = `url('https://images.unsplash.com/photo-1491821893533-80f535044695?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a57657772157dd7a5a6fb827ab9fba23&auto=format&fit=crop&w=934&q=80')`;
 
-bgImg = `url('https://images.unsplash.com/photo-1491821893533-80f535044695?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a57657772157dd7a5a6fb827ab9fba23&auto=format&fit=crop&w=934&q=80')`;
+    // DOM
+    this.appBg = document.getElementsByClassName('app-background')[0];
+    this.appBgImg = document.getElementsByClassName('app-background-image')[0];
+    this.body = document.getElementsByTagName('body')[0];
+  }
 
+  static toggleBgOpacity() {
+    let o = this.appBg.style.opacity;
+    o === '1' ? o = '0' : ''
+  }
 
-// DOM
-const appBg = document.getElementsByClassName('app-background')[0];
-const appBgImg = document.getElementsByClassName('app-background-image')[0];
-const body = document.getElementsByTagName('body')[0];
+  handleScrollUp() {
+    if (window.scrollY < 200 ) {
+      body.classList.remove('theme-dark');
+      appBg.style.backgroundColor = cream;
+      this.toggleBgOpacity();
+    }
 
-function init() {
-  appBg.style.backgroundColor = cream;
-  appBgImg.style.backgroundImage = bgImg;
-  appBgImg.style.opacity = `0`;
+    if (window.scrollY > 600 ) {
+      body.classList.add('theme-dark');
+      appBg.style.backgroundColor = tomato;
+      this.toggleBgOpacity();
+    }
+  }
+  
+  addScrollListener() {
+    document.addEventListener('scroll', function() {
+      console.log(window.scrollY);
+      this.handleScrollUp();
+    })
+  }
+
+  test() {
+    console.log(this.cream + ' is cream');
+  }
+
+  init() {
+    this.appBg.style.backgroundColor = this.cream;
+    this.appBgImg.style.backgroundImage = this.bgImg;
+    this.appBgImg.style.opacity = `1`;
+
+    this.addScrollListener();
+  }
 }
 
-
-document.addEventListener('scroll', function() {
-  console.log(window.scrollY);
-  
-  if (window.scrollY < 200 ) {
-    body.classList.remove('theme-dark');
-    appBg.style.backgroundColor = cream;
-  }
-
-  if (window.scrollY > 600 ) {
-    body.classList.add('theme-dark');
-    appBg.style.backgroundColor = tomato;
-  }
-})
-  
-init();
+const w = new WWA;
+w.init();
